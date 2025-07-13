ccaeafc6313c30b434870ec45dd3195e3780146a
Daria Fedoseeva <dashafedoseeva@yandex.ru>
2025-07-13 23:54:41 +0300

    sumSalary(salaries)

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,23 @@
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+}
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let key in salaries) {
+    let value = salaries[key];
+    if (
+      typeof value === 'number' &&
+      value !== Infinity &&
+      value !== -Infinity &&
+      value === value //проверка на NaN(т к NaN === NaN дает нам false(просто запомнить), т к логическое и - у нас не выполнится блок if, потому что одно из условий false)
+    ) {
+      sum += value;
+    }
+  }
+  return sum;
 }
