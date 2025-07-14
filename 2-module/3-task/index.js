40d9ecbcbe10b1866a939da7cf809fafe3b4aebc
Daria Fedoseeva <dashafedoseeva@yandex.ru>
2025-07-14 23:46:05 +0300

    calculator

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,14 @@
 let calculator = {
-  // ваш код
+  read(a, b) {
+    this.a = a //this указывает на calculator, т е как пример calculator.a = 3, сохраняем в объекте calculator
+    this.b = b
+  },
+  sum() {
+    return this.a + this.b // забираем из объекта
+  },
+  mul() {
+    return this.a * this.b
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
