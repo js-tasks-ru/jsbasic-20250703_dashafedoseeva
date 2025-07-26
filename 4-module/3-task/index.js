f2dec27bf95d00e2b36b8d17d88f9f7a137b3a4a
Daria Fedoseeva <dashafedoseeva@yandex.ru>
2025-07-26 23:14:54 +0300

    4-module/3-task

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,30 @@
 function highlight(table) {
-  // ваш код...
+  for (let row of table.tBodies[0].rows) {
+    const ageCell = row.cells[1];    // Ячейка age
+    const genderCell = row.cells[2]; // Ячейка gender
+    const statusCell = row.cells[3]; // Ячейка status
+    
+    // 1. Статус: проверяем наличие data-available
+    if (statusCell.hasAttribute('data-available')) {
+      const isAvailable = statusCell.dataset.available === "true";
+
+      // Добавляем класс в зависимости от значения
+      row.classList.add(isAvailable ? 'available' : 'unavailable');
+    } else {
+      // если нет атрибута скрываем строку
+      row.hidden = true;
+    }
+
+    // 2. Пол: m — male, f — female
+    if (genderCell.textContent === 'm') {
+      row.classList.add('male');
+    } else if (genderCell.textContent === 'f') {
+      row.classList.add('female');
+    }
+
+    // 3. Возраст: если меньше 18 — зачёркиваем
+    if (parseInt(ageCell.textContent) < 18) {
+      row.style.textDecoration = 'line-through';
+    }
+  }
 }
